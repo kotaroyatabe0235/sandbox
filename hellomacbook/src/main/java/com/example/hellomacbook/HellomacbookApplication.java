888e4044ca4013f0ebdecebd4d24d071bbc7f02b
kotaroyatabe <kotaroyatabe.dev@gmail.com>
2022-12-31 13:08:59 +0000

    add hellomacbook project

---
+++ hellomacbook/src/main/java/com/example/hellomacbook/HellomacbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellomacbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellomacbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellomacbookApplication.class, args);
+	}
+
+}
---
+++ hellomacbook/src/test/java/com/example/hellomacbook/HellomacbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellomacbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellomacbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
