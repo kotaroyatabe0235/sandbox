6662a2cbb5a711b2aa50140806fc2dd1afdc3f2a
kotaroyatabe <kotaroyatabe.dev@gmail.com>
2023-01-03 18:09:26 +0900

    R.I.P reddis

--- messaging-redis/src/main/java/com/example/messagingredis/MessagingRedisApplication.java
+++ messaging-redis/src/main/java/com/example/messagingredis/MessagingRedisApplication.java
@@ -1,10 +1,27 @@
 package com.example.messagingredis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @SpringBootApplication
 public class MessagingRedisApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MessagingRedisApplication.class);
+	
+	@Bean
+	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+		return container;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingRedisApplication.class, args);
