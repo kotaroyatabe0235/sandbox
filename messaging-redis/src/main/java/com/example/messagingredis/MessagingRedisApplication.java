19a5fe132f2ce4eb6787002ebb6d66602e0a8c79
kotaroyatabe <kotaroyatabe.dev@gmail.com>
2023-01-05 22:18:37 +0900

    W.I.P

--- messaging-redis/src/main/java/com/example/messagingredis/MessagingRedisApplication.java
+++ messaging-redis/src/main/java/com/example/messagingredis/MessagingRedisApplication.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
@@ -23,8 +25,31 @@ public class MessagingRedisApplication {
 		return container;
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(MessagingRedisApplication.class, args);
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	@Bean
+	Receiver receiver() {
+		return new Receiver();
+	}
+
+	@Bean
+	StringRedisTemplate	template(RedisConnectionFactory connectionFactory) {
+		return new StringRedisTemplate(connectionFactory);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		ApplicationContext ctx = SpringApplication.run(MessagingRedisApplication.class, args);
+		StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
+		Receiver receiver = ctx.getBean(Receiver.class);
+		while(receiver.getCount() == 0) {
+			LOGGER.info("Sending message...");
+			template.convertAndSend("chat", "Hello from Redis!");
+			Thread.sleep(500L);
+		}
+		System.exit(0);
 	}
 
 }
