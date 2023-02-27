d3e8e095c46826f96ce56be82a753f78db8428ab
kotaroyatabe <kotaroyatabe.dev@gmail.com>
2023-02-27 22:11:53 +0900

    2023_02_27

--- todolist/src/main/java/com/example/todolist/entity/Todo.java
+++ todolist/src/main/java/com/example/todolist/entity/Todo.java
@@ -22,4 +22,21 @@ public class Todo {
     this();
     this.title = title; 
   }
+  
+  public void setTitle(String title) {
+    this.title = title;  
+  }
+
+  public void setCompleted(boolean completed) {
+    this.completed = completed;
+  }
+
+  public String getTitle() {
+    return this.title; 
+  }
+  
+  public boolean getCompleted() {
+    return this.completed; 
+  }
+
 }
