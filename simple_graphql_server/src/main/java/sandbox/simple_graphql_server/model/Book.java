f1018f52a8fdcff4dcc6b578d11def968603fdca
kotaroyatabe <kotaroyatabe.dev@gmail.com>
2023-12-08 08:51:51 +0900

    2023/12/08

---
+++ simple_graphql_server/src/main/java/sandbox/simple_graphql_server/model/Book.java
@@ -0,0 +1,16 @@
+package sandbox.simple_graphql_server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Book {
+    
+    private String id;
+
+    private String name;
+
+    private int pageCount;
+
+}
